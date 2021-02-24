5a91968684e892f5329863f2ed8cf31fa4bcc052
Jean-Peer Lorenz <peer.loz@gmx.net>
2021-02-24 06:33:51 +0100

    FIX: improve error handling when fetching picture of day
    Replace generic exception handling code by catching network errors
    specifically. Also try to fetch an older picture in case of a network
    error.
    
    During testing it happened, that a request for a specific date returned
    404, since no picture for that day was yet available.

--- app/src/main/java/com/udacity/asteroidradar/repository/PictureOfDayRepository.kt
+++ app/src/main/java/com/udacity/asteroidradar/repository/PictureOfDayRepository.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
 import com.udacity.asteroidradar.Constants
 import com.udacity.asteroidradar.api.NasaApiService
+import com.udacity.asteroidradar.api.NetworkPictureOfDay
 import com.udacity.asteroidradar.api.asDatabaseModel
 import com.udacity.asteroidradar.database.PictureOfDayDao
 import com.udacity.asteroidradar.database.asDomainModel
@@ -28,31 +29,43 @@ class PictureOfDayRepository(
      * Refresh the picture of the day stored in the offline cache.
      * It will load a picture of one of the previous days in case
      * today's PoD is actually a video.
+     *
+     * In case of network errors (e.g. some cases return 404 for a given date if not yet
+     * a picture is available), it also tries to fetch an older picture.
      */
     suspend fun refresh() {
         withContext(Dispatchers.IO) {
-            try {
-                val today = getTodayDateFormatted()
+            val today = getTodayDateFormatted()
 
-                for (offset in 0 downTo -1 * Constants.NUMBER_OF_RETRIES_APOD) {
-
-                    val date = getOffsetDateFormatted(date = today, offsetInDays = offset)
+            for (offset in 0 downTo -1 * Constants.NUMBER_OF_RETRIES_APOD) {
+                val date = getOffsetDateFormatted(date = today, offsetInDays = offset)
+                try {
                     val networkPictureOfDay = nasaService.getPictureOfTheDay(date)
-
-                    if (networkPictureOfDay.mediaType == "image") {
-                        dao.insertPictureOfDay(networkPictureOfDay.asDatabaseModel())
+                    val success = maybeInsertIntoDatabase(networkPictureOfDay)
+                    if (success) {
                         break
-                    } else {
-                        Log.i(
-                            "PictureOfDayRepository",
-                            "Picture of day ignored (media type = ${networkPictureOfDay.mediaType})."
-                        )
                     }
+                } catch (e: retrofit2.HttpException) {
+                    Log.e("PictureOfDayRepository", "Network error: $e")
                 }
-            } catch (e: Exception) {
-                Log.e("PictureOfDayRepository", e.toString())
-                throw e
             }
         }
+    }
+
+    private suspend fun maybeInsertIntoDatabase(networkPictureOfDay: NetworkPictureOfDay): Boolean {
+        if (networkPictureOfDay.mediaType == "image") {
+            dao.insertPictureOfDay(networkPictureOfDay.asDatabaseModel())
+            Log.i(
+                "PictureOfDayRepository",
+                "Picture of day stored (${networkPictureOfDay})."
+            )
+            return true
+        } else {
+            Log.i(
+                "PictureOfDayRepository",
+                "Picture of day ignored (media type = ${networkPictureOfDay.mediaType})."
+            )
+        }
+        return false
     }
 }
