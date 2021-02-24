76b99ed3fd147c6ad9dcaf873f8941757324cbce
Jean-Peer Lorenz <peer.loz@gmx.net>
2021-02-24 05:59:27 +0100

    FIX: fix unittests and update use of constants

--- app/src/test/java/com/udacity/asteroidradar/UtilsKtTest.kt
+++ app/src/test/java/com/udacity/asteroidradar/UtilsKtTest.kt
@@ -8,7 +8,7 @@ class UtilsKtTest {
 
     @Test
     fun getNextSevenDaysFormattedDates_correctNumberOfDates() {
-        val nextSevenDaysFormattedDates = getNextSevenDaysFormattedDates()
+        val nextSevenDaysFormattedDates = getNextDaysFormattedDates(Constants.NEO_FEED_DATE_LIMIT)
         // should return list of dates like ["2020-12-02","2020-12-03",...]
         // list should contain 8 dates: today plus next 7 days
         assertEquals(8, nextSevenDaysFormattedDates.size)
@@ -17,7 +17,7 @@ class UtilsKtTest {
     @Test
     fun getNextWeekEndDateFormatted_correctEndDate() {
         val start = "2020-12-02"
-        val end = getNextWeekEndDateFormatted(start)
+        val end = getOffsetDateFormatted(start, Constants.DAYS_IN_WEEK)
         assertEquals("2020-12-09", end)
     }
 
